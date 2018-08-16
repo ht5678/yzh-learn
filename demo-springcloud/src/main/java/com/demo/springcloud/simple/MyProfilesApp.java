8c902c8eea2d5a891b31bc7438d14c9728a96afd
yue <sdwhyzhh@163.com>
2018-08-16 20:23:36 +0800

    临时提交

---
+++ demo-springcloud/src/main/java/com/demo/springcloud/simple/MyProfilesApp.java
@@ -0,0 +1,41 @@
+package com.demo.springcloud.simple;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * 
+ * 
+ *         ┏┓　　　┏┓
+ *      ┏┛┻━━━┛┻┓
+ *      ┃　　　　　　　┃ 　
+ *      ┃　　　━　　　┃
+ *      ┃　┳┛　┗┳　┃
+ *      ┃　　　　　　　┃
+ *      ┃　　　┻　　　┃
+ *      ┃　　　　　　　┃
+ *      ┗━┓　　　┏━┛
+ *         ┃　　　┃　　　　　　　　　
+ *         ┃　　　┃
+ *         ┃　　　┗━━━┓
+ *         ┃　　　　　　　┣┓
+ *         ┃　　　　　　　┏┛
+ *         ┗┓┓┏━┳┓┏┛
+ *　　      ┃┫┫　┃┫┫
+ *　        ┗┻┛　┗┻┛
+ *
+ *-------------------- 神兽保佑 永无bug --------------------
+ * 
+ * 
+ * 
+ * @author yuezh2   2018年8月16日 下午7:49:03
+ *
+ */
+public class MyProfilesApp {
+	
+	
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(MyProfilesApp.class)
+				.properties("spring.profiles.active=linux").run(args);
+	}
+
+}
