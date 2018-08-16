6a5375925e654b0cc203792956988f7f083843b5
yue <sdwhyzhh@163.com>
2018-08-16 18:09:08 +0800

    hello world的web程序

---
+++ demo-springcloud/src/main/java/com/demo/springcloud/simple/MyController.java
@@ -0,0 +1,46 @@
+package com.demo.springcloud.simple;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 
+ * 
+ *         ┏┓　　　┏┓
+ *      ┏┛┻━━━┛┻┓
+ *      ┃　　　　　　　┃ 　
+ *      ┃　　　━　　　┃
+ *      ┃　┳┛　┗┳　┃
+ *      ┃　　　　　　　┃
+ *      ┃　　　┻　　　┃
+ *      ┃　　　　　　　┃
+ *      ┗━┓　　　┏━┛
+ *         ┃　　　┃　　　　　　　　　
+ *         ┃　　　┃
+ *         ┃　　　┗━━━┓
+ *         ┃　　　　　　　┣┓
+ *         ┃　　　　　　　┏┛
+ *         ┗┓┓┏━┳┓┏┛
+ *　　      ┃┫┫　┃┫┫
+ *　        ┗┻┛　┗┻┛
+ *
+ *-------------------- 神兽保佑 永无bug --------------------
+ * 
+ * 
+ * 
+ * @author yuezh2   2018年8月16日 下午4:59:56
+ *
+ */
+@Controller
+public class MyController {
+
+	
+	
+	@ResponseBody
+	@RequestMapping(value="hello")
+	public String hello(){
+		return "hello world";
+	}
+	
+}
