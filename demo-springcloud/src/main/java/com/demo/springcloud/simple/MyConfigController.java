33cc3781fa130301779299a002b59c0598688f0d
yue <sdwhyzhh@163.com>
2018-08-16 19:42:04 +0800

    配置文件demo提交

--- demo-springcloud/src/main/java/com/demo/springcloud/simple/MyConfigController.java
+++ demo-springcloud/src/main/java/com/demo/springcloud/simple/MyConfigController.java
@@ -30,6 +30,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  *-------------------- 神兽保佑 永无bug --------------------
  * 
  * 
+ * 自定义配置文件启动
  * 
  * @author yuezh2   2018年8月16日 下午7:15:51
  *
@@ -41,11 +42,27 @@ public class MyConfigController {
 	@Autowired
 	private ApplicationContext ctx;
 	
+	/**
+	 * 
+	 * @return
+	 */
 	@ResponseBody
 	@RequestMapping(value="prop")
 	public String getName(){
 		
 		return ctx.getEnvironment().getProperty("test.user.name");
+	}
+	
+	
+	/**
+	 * 
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value="prop1")
+	public String getName1(){
+		
+		return ctx.getEnvironment().getProperty("test.user.name1");
 	}
 	
 	
