73d2a129d5afa950bfbfecf4d34afbdd7d88e755
yue <sdwhyzhh@163.com>
2018-08-19 20:24:20 +0800

    结构变更提交

---
+++ demo-springcloud-all/first-114/src/main/java/com/demo/sc/first/ServerApp.java
@@ -0,0 +1,44 @@
+package com.demo.sc.first;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+ * 
+ *         ┏┓　　　┏┓
+ *      ┏┛┻━━━┛┻┓
+ *      ┃　　　　　　　┃ 　
+ *      ┃　　　━　　　┃
+ *      ┃　┳┛　┗┳　┃
+ *      ┃　　　　　　　┃
+ *      ┃　　　┻　　　┃
+ *      ┃　　　　　　　┃
+ *      ┗━┓　　　┏━┛
+ *         ┃　　　┃　　　　　　　　　
+ *         ┃　　　┃
+ *         ┃　　　┗━━━┓
+ *         ┃　　　　　　　┣┓
+ *         ┃　　　　　　　┏┛
+ *         ┗┓┓┏━┳┓┏┛
+ *　　      ┃┫┫　┃┫┫
+ *　        ┗┻┛　┗┻┛
+ *
+ *-------------------- 神兽保佑 永无bug --------------------
+ * 
+ * 
+ * 
+ * @author yuezh2   2018年8月17日 下午3:17:11
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerApp {
+	
+	
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ServerApp.class).web(true).run(args);
+	}
+
+}
