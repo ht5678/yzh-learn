1997155d31298a5ed75862bec378e3b3bb0739ef
yue <sdwhyzhh@163.com>
2018-08-27 20:30:04 +0800

    zuul介绍与使用demo提交

---
+++ demo-springcloud-all/S3-l01-zuul-first-router/src/main/java/com/demo/springcloud/zuul/router/RouterApp.java
@@ -0,0 +1,48 @@
+package com.demo.springcloud.zuul.router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 
+ * 
+ *         ┏┓　　　┏┓
+ *      ┏┛┻━━━┛┻┓
+ *      ┃　　　　　　　┃ 　
+ *      ┃　　　━　　　┃
+ *      ┃　┳┛　┗┳　┃
+ *      ┃　　　　　　　┃
+ *      ┃　　　┻　　　┃
+ *      ┃　　　　　　　┃
+ *      ┗━┓　　　┏━┛
+ *         ┃　　　┃　　　　　　　　　
+ *         ┃　　　┃
+ *         ┃　　　┗━━━┓
+ *         ┃　　　　　　　┣┓
+ *         ┃　　　　　　　┏┛
+ *         ┗┓┓┏━┳┓┏┛
+ *　　      ┃┫┫　┃┫┫
+ *　        ┗┻┛　┗┻┛
+ *
+ *-------------------- 神兽保佑 永无bug --------------------
+ * 
+ * 
+ * 
+ * @author yuezh2   2018年8月27日 下午5:51:58
+ *
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class RouterApp {
+
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(RouterApp.class, args);
+	}
+	
+}
---
+++ demo-springcloud-all/S3-l01-zuul-first-server/src/main/java/com/demo/springcloud/zuul/server/ServerApp.java
@@ -0,0 +1,65 @@
+package com.demo.springcloud.zuul.server;
+
+import org.apache.catalina.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * 
+ *         ┏┓　　　┏┓
+ *      ┏┛┻━━━┛┻┓
+ *      ┃　　　　　　　┃ 　
+ *      ┃　　　━　　　┃
+ *      ┃　┳┛　┗┳　┃
+ *      ┃　　　　　　　┃
+ *      ┃　　　┻　　　┃
+ *      ┃　　　　　　　┃
+ *      ┗━┓　　　┏━┛
+ *         ┃　　　┃　　　　　　　　　
+ *         ┃　　　┃
+ *         ┃　　　┗━━━┓
+ *         ┃　　　　　　　┣┓
+ *         ┃　　　　　　　┏┛
+ *         ┗┓┓┏━┳┓┏┛
+ *　　      ┃┫┫　┃┫┫
+ *　        ┗┻┛　┗┻┛
+ *
+ *-------------------- 神兽保佑 永无bug --------------------
+ * 
+ * 
+ * 
+ * @author yuezh2   2018年8月27日 下午5:43:34
+ *
+ */
+@SpringBootApplication
+@RestController
+public class ServerApp {
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApp.class, args);
+	}
+	
+	
+	
+	/**
+	 * 
+	 * @param name
+	 * @return
+	 */
+	@RequestMapping(value="/hello/{name}",method=RequestMethod.GET)
+	public String hello(@PathVariable String name){
+		return "hello"+name;
+	}
+	
+
+}
