25e86d437ad6a2cb07a9466fef276de41108b0c5
yue <sdwhyzhh@163.com>
2018-09-01 22:24:10 +0800

    springcloud整合rabbitmq练习提交

---
+++ demo-springcloud-all/S3-l08-springcloud-stream-producer/src/main/java/com/demo/springcloud/stream/rabbitmq/ProducerApp.java
@@ -0,0 +1,45 @@
+package com.demo.springcloud.stream.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 
+ * 
+ *         ┏┓　　　┏┓
+ *      ┏┛┻━━━┛┻┓
+ *      ┃　　　　　　　┃ 　
+ *      ┃　　　━　　　┃
+ *      ┃　┳┛　┗┳　┃
+ *      ┃　　　　　　　┃
+ *      ┃　　　┻　　　┃
+ *      ┃　　　　　　　┃
+ *      ┗━┓　　　┏━┛
+ *         ┃　　　┃　　　　　　　　　
+ *         ┃　　　┃
+ *         ┃　　　┗━━━┓
+ *         ┃　　　　　　　┣┓
+ *         ┃　　　　　　　┏┛
+ *         ┗┓┓┏━┳┓┏┛
+ *　　      ┃┫┫　┃┫┫
+ *　        ┗┻┛　┗┻┛
+ *
+ *-------------------- 神兽保佑 永无bug --------------------
+ * 
+ * 
+ * 
+ * @author yuezh2   2018年9月1日 下午8:09:32
+ *
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ProducerApp {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApp.class, args);
+	}
+
+	
+}
---
+++ demo-springcloud-all/S3-l08-springcloud-stream-server/src/main/java/com/demo/springcloud/stream/rabbitmq/ServerApp.java
@@ -0,0 +1,48 @@
+package com.demo.springcloud.stream.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+ * 
+ *         ┏┓　　　┏┓
+ *      ┏┛┻━━━┛┻┓
+ *      ┃　　　　　　　┃ 　
+ *      ┃　　　━　　　┃
+ *      ┃　┳┛　┗┳　┃
+ *      ┃　　　　　　　┃
+ *      ┃　　　┻　　　┃
+ *      ┃　　　　　　　┃
+ *      ┗━┓　　　┏━┛
+ *         ┃　　　┃　　　　　　　　　
+ *         ┃　　　┃
+ *         ┃　　　┗━━━┓
+ *         ┃　　　　　　　┣┓
+ *         ┃　　　　　　　┏┛
+ *         ┗┓┓┏━┳┓┏┛
+ *　　      ┃┫┫　┃┫┫
+ *　        ┗┻┛　┗┻┛
+ *
+ *-------------------- 神兽保佑 永无bug --------------------
+ * 
+ * 
+ * 
+ * @author yuezh2   2018年8月27日 下午9:11:09
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerApp {
+	
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApp.class, args);
+	}
+
+}
