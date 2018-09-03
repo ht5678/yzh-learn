ec33fc70d152760c2f93637c7819ba85030f783b
yue <sdwhyzhh@163.com>
2018-09-03 21:56:44 +0800

    练习提交

--- demo-springcloud-all/S4-l02-springcloud-config-server/src/main/java/com/demo/springcloud/config/ConfigApp.java
+++ demo-springcloud-all/S4-l02-springcloud-config-server/src/main/java/com/demo/springcloud/config/ConfigApp.java
@@ -1,7 +1,7 @@
 package com.demo.springcloud.config;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -42,7 +42,7 @@ public class ConfigApp {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(ConfigApp.class).web(true).run(args);
+		SpringApplication.run(ConfigApp.class, args);
 	}
 
 }
