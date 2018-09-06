ee492259b4abea15375b21ab86cfdfefaea9270c
yue <sdwhyzhh@163.com>
2018-09-06 20:54:24 +0800

    修改配置文件提交

--- demo-springcloud-all/S4-l05-springcloud-eureka-zuul/src/main/java/com/demo/springcloud/config/zuul/ZuulApp.java
+++ demo-springcloud-all/S4-l05-springcloud-eureka-zuul/src/main/java/com/demo/springcloud/config/zuul/ZuulApp.java
@@ -1,5 +1,9 @@
 package com.demo.springcloud.config.zuul;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * 
@@ -8,6 +12,17 @@ package com.demo.springcloud.config.zuul;
  * @author yuezh2   2018年9月6日 下午6:19:58
  *
  */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class ZuulApp {
 
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ZuulApp.class).run(args);
+	}
 }
