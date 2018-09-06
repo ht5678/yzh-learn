9a86d56fc73a2a45c6a1017591cac61c6a7d0084
yue <sdwhyzhh@163.com>
2018-09-06 22:01:03 +0800

    springcloud整合配置中心练习达成

--- demo-springcloud-all/S4-l05-springcloud-eureka-bus/src/main/java/com/demo/springcloud/bus/TestRefresh.java
+++ demo-springcloud-all/S4-l05-springcloud-eureka-bus/src/main/java/com/demo/springcloud/bus/TestRefresh.java
@@ -44,8 +44,11 @@ public class TestRefresh {
 	 */
 	public static void main(String[] args) throws Exception{
 		//端口号是要更新配置的客户端的端口号
+		//curl 10.116.28.242:10000/actuator/bus-refresh -d '' -H 'Content-Type:application/json'
+		//这个执行失败是因为需要添加header           Content-Type:application/json
 		CloseableHttpClient client = HttpClients.createDefault();
 		HttpPost post = new HttpPost("http://localhost:10000/actuator/bus-refresh");
+		//TODO:添加header
 		HttpResponse response = client.execute(post);
 		System.out.println(EntityUtils.toString(response.getEntity()));
 	}
