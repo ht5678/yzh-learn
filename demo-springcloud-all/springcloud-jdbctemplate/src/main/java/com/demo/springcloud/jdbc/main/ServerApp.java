2de647353e7f9470d63286cfa508bf481c2a1071
yue <sdwhyzhh@163.com>
2018-09-20 16:46:53 +0800

    注释提交

--- demo-springcloud-all/springcloud-jdbctemplate/src/main/java/com/demo/springcloud/jdbc/main/ServerApp.java
+++ demo-springcloud-all/springcloud-jdbctemplate/src/main/java/com/demo/springcloud/jdbc/main/ServerApp.java
@@ -2,16 +2,24 @@ package com.demo.springcloud.jdbc.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * 
- * 
+ * 首先要将spring boot自带的DataSourceAutoConfiguration禁掉，因为它会读取application.properties文件的spring.datasource.*属性并自动配置单数据源。在@SpringBootApplication注解中添加exclude属性即可：
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class
+})
  * 
  * @author yuezh2   2018年9月18日 下午5:28:52
  *
  */
-@SpringBootApplication(scanBasePackages={"com.demo.springcloud.jdbc"})
+@SpringBootApplication(scanBasePackages={"com.demo.springcloud.jdbc"},
+exclude = {
+        DataSourceAutoConfiguration.class
+})
 @EnableEurekaServer
 public class ServerApp {
 	
