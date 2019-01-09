434fe60bc1a7304352e0da6e8435e11f71868130
yuezh2 <yuezh2@lenovo.com>
2019-01-09 15:36:50 +0800

    单例创建对象只初始化一次

--- demo-python/base/hm_56_singleton.py
+++ demo-python/base/hm_56_singleton.py
@@ -2,13 +2,20 @@ class MusicPlayer:
 
     instance = None;
 
+    initFlag = False;
+
+
     def __new__(cls, *args, **kwargs):
         if cls.instance is None:
             cls.instance = super().__new__(cls);
         return cls.instance;
 
     def __init__(self):
+        if MusicPlayer.initFlag:
+            return
+
         print("init ... ");
+        MusicPlayer.initFlag=True;
 
 
 mp1 = MusicPlayer();
