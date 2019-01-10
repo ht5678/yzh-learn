2501e0f3a9d61944e32ef1e98439770c966744a7
yuezh2 <yuezh2@lenovo.com>
2019-01-10 10:54:20 +0800

    主动抛出异常

---
+++ demo-python/base/hm_71_raiseException.py
@@ -0,0 +1,14 @@
+def inputPassword():
+    pwd = input("请输入密码:");
+
+    if len(pwd)>=8:
+        return pwd;
+
+    print("主动跑出异常");
+
+    ex = Exception("密码长度不够");
+
+    raise ex;
+
+
+print(inputPassword());
