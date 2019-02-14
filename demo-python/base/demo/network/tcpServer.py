e31246ae3cbf9cc2cc701740de2e2984a6d50914
yuezh2 <yuezh2@lenovo.com>
2019-02-14 15:34:01 +0800

    tcp练习提交

---
+++ demo-python/base/demo/network/tcpServer.py
@@ -0,0 +1,36 @@
+import socket;
+
+
+
+def execute():
+    #1.创建套接字socket
+    tcpServerSocket = socket.socket(socket.AF_INET , socket.SOCK_STREAM);
+
+    #2.绑定本地信息bind
+    tcpServerSocket.bind(("",7890));
+
+    #3.让默认的套接字由主动变为被动
+    tcpServerSocket.listen(128);
+
+    while True:
+
+        #4.等待客户端的连接 accept
+        newClientSocket , clientAddr = tcpServerSocket.accept();
+
+        while True:
+
+            #5.接收客户端发来的请求     recv()和recv_from()方法的不同在于是否有客户端数据
+            recvData = newClientSocket.recv(1024);
+            print("客户端发过来的请求是%s "% recvData.decode("utf-8"));
+
+            #回送一部分数据给客户端
+            newClientSocket.send("hahaha".encode("utf-8"));
+
+        #关闭套接字
+        newClientSocket.close();
+    tcpServerSocket.close();
+
+
+
+if __name__ == '__main__':
+    execute();
