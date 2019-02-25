1abcf76f97517b6c6d461b59e1fd2d094169e4d0
yuezh2 <yuezh2@lenovo.com>
2019-02-25 16:49:58 +0800

    迭代器练习提交

---
+++ demo-python/base/demo/basic/IteratorDemo.py
@@ -0,0 +1,48 @@
+import time;
+from collections.abc import  Iterable
+from collections.abc import  Iterator
+
+
+class Classmate(object):
+    def __init__(self):
+        self.names = list();
+
+
+    def add(self , name):
+        self.names.append(name);
+
+
+    def __iter__(self):
+        """如果想要一个对象称为一个   可以迭代的对象 , 即可以使用for , 那么必须实现 __iter__方法"""
+        return ClassIterator(self);
+
+
+
+class ClassIterator(object):
+
+    def __init__(self , obj):
+        self.obj = obj;
+
+    def __iter__(self):
+        pass;
+
+    def __next__(self):
+        return self.obj.names[0];
+
+
+
+classmate = Classmate();
+classmate.add("张三");
+classmate.add("李四");
+classmate.add("王五");
+
+
+print("判断classmate是否可以迭代的对象:" , isinstance(classmate  , Iterable));
+classmate_iterator = iter(classmate);
+print("判断classmate_iterator是否是迭代器:" , isinstance(classmate_iterator , Iterator));
+print(next(classmate_iterator));
+
+
+for name in classmate:
+    print(name);
+    time.sleep(1);
