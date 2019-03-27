f466de132c9aaca7404bcde3a2170d00be506972
yuezh2 <yuezh2@lenovo.com>
2019-03-27 20:29:47 +0800

    property练习提交

--- demo-python/base/demo/basic/CustomList.py
+++ demo-python/base/demo/basic/CustomList.py
@@ -1,17 +1,48 @@
 
-class Foo:
+
+"""
+
+https://docs.python.org/2/reference/datamodel.html#object.__getslice__
 
 
-    def __getslice__(self , i , j):
-        print("__getslice__",i,j);
 
 
-    def __setslice__(self, i, j, sequence):
-        print("__setslice__" , i , j);
+class MyClass:
+    ...
+    def __getitem__(self, index):
+        ...
+    def __setitem__(self, index, value):
+        ...
+    def __delitem__(self, index):
+        ...
+
+    if sys.version_info < (2, 0):
+        # They won't be defined if version is at least 2.0 final
+
+        def __getslice__(self, i, j):
+            return self[max(0, i):max(0, j):]
+        def __setslice__(self, i, j, seq):
+            self[max(0, i):max(0, j):] = seq
+        def __delslice__(self, i, j):
+            del self[max(0, i):max(0, j):]
+    ...
+
+"""
 
 
-    def __delslice__(self, i, j):
-        print("__delslice__" , i , j);
+class Foo(object):
+
+
+    def __getitem__(self, item):
+        print("__getslice__",item);
+
+
+    def __setitem__(self, key, value):
+        print("__setslice__" , key , value);
+
+
+    def __delitem__(self, key):
+        print("__delslice__" , key);
 
 
 obj = Foo();
