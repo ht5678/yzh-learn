3d09d7b0986507d0db0c769ed5a47bce0988a16b
yuezh2 <yuezh2@lenovo.com>
2019-04-03 17:40:18 +0800

    06-装饰器-6-对应有返回值函数进行装饰、通用装饰器

--- demo-python/base/demo/basic/DecorateDemo.py
+++ demo-python/base/demo/basic/DecorateDemo.py
@@ -4,7 +4,7 @@ import time;
 def setFunc(func):
     def callFunc(*args , **kwargs):
         startTime = time.time();
-        func(*args , **kwargs);
+        return func(*args , **kwargs);
         stopTime= time.time();
         print("all times %f" % (stopTime-startTime));
     return callFunc;
@@ -17,10 +17,12 @@ def test(a , *args , **kwargs):
     print("------test1------ " , kwargs);
     # for i in range(1000):
     #     pass;
+    return "ok";
 
 
 test(100);
 
 test(100,200);
 
-test(100,200,300 , mm =100);
+ret  = test(100,200,300 , mm =100);
+print(ret);
