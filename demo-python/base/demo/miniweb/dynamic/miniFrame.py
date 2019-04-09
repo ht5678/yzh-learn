b29de44f7086438574ebc4504e4db1fc019bc9af
yuezh2 <yuezh2@lenovo.com>
2019-04-09 18:11:22 +0800

    05-用装饰器完成路由功能-4

--- demo-python/base/demo/miniweb/dynamic/miniFrame.py
+++ demo-python/base/demo/miniweb/dynamic/miniFrame.py
@@ -26,13 +26,34 @@ def application(fileName):
 """
 
 
+URL_FUNC_DICT={
+
+};
+
+
+def route(url):
+    def setFunc(func):
+        #URL_FUNC_DICT['/index.py']=index
+        URL_FUNC_DICT[url] = func;
+        def callFunc(*args , **kwargs):
+            return func(*args,**kwargs);
+        return callFunc;
+    return setFunc;
+
+
+@route("/index.py")
 def index():
     with open("html/index.html") as f:
         content = f.read();
     return content;
 
+
+@route("/login.py")
 def login():
     return "这是登录页面";
+
+
+
 
 
 #wsgi协议支持
@@ -40,9 +61,16 @@ def application(environ , startResponse):
     startResponse('200 OK' , [('Content-Type' , 'text/html;charset=utf-8'),('server','mini-frameV1.0')]);
     fileName = environ['PATH_INFO'];
 
-    if fileName=='/index.py':
-        return index();
-    elif fileName=='/login.py':
-        return login();
-    else:
-        return "Hello World!";
+    """
+        if fileName=='/index.py':
+            return index();
+        elif fileName=='/login.py':
+            return login();
+        else:
+            return "Hello World!";
+    """
+    try:
+        func = URL_FUNC_DICT[fileName];
+        return func();
+    except Exception as ret:
+        return "产生异常:%s" % str(ret);
