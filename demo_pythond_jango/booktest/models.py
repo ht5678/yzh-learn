25d6a46a98e474c1ca7f0729c2f449818a7a0c06
yuezh2 <yuezh2@lenovo.com>
2019-09-02 20:17:24 +0800

    练习提交

--- demo_pythond_jango/booktest/models.py
+++ demo_pythond_jango/booktest/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+#图书类
+class BookInfo(models.Model):
+    '''图书模型类'''
+    #CharField表示一个字符串,max_length表示字符串的最大长度
+    btitle = models.CharField(max_length=20);
+    #出版日期,DateField说明是一个日期类
+    bpub_date=models.DateField();
