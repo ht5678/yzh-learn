fdcfdd339f4264d1fe4dc1a6ea5a0ec2eae7eb7f
yuezh2 <yuezh2@lenovo.com>
2019-09-05 21:05:46 +0800

    登录2 临时提交

--- demo_pythond_jango/logindemo/urls.py
+++ demo_pythond_jango/logindemo/urls.py
@@ -5,7 +5,12 @@ from logindemo import views
 #在应用的urls文件中进行url配置的时间
 #1.严格匹配开头和结尾
 urlpatterns=[
+
+    url(r'^index',views.index),
+
     url(r'^login',views.login),  #跳转到登录页面
+
+    url(r'^login_check',views.login_check),     #用户登录校验
 
 
 
--- demo_pythond_jango/logindemo/views.py
+++ demo_pythond_jango/logindemo/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+import logging
 # Create your views here.
+
+
+
+def index(request):
+    return render(request, 'logindemo/index.html')
 
 
 #http://localhost:8000/login/login
@@ -11,6 +16,27 @@ def login(request):
 
 def login_check(request):
     '''登录校验视图'''
+    #request.POST保存的是post方式提交的参数    QueryDict
+    #request.GET保存的是get方式提交的参数
     #1.获取提交的用户名和密码
+    username = request.POST.get('username');
+    password = request.POST.get('password');
+
+    logging.info(username)
+    print('username:'+username)
+    print('password:'+password)
+
     #2.进行登录的校验
+    #实际开发,根据用户名和密码查找数据库
+    #模拟:
+    if username=='smart' and password=='123':
+        #用户名密码正确 , 跳转到首页
+        #return redirect('/login/index');
+        print('username:' + username)
+        print('password:' + password)
+    else:
+        #用户名密码错误 , 跳转到登录页面
+        #return redirect('/login/login');
+        print('usernam1e:' + username)
+        print('password1:' + password)
     #3.返回应答
