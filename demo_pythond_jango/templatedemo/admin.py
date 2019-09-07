e96775bffc088b63fb9a3cec16f08cb607a603f1
yuezh2 <yuezh2@lenovo.com>
2019-09-07 22:53:02 +0800

    06_后台管理_编辑页选项

--- demo_pythond_jango/templatedemo/admin.py
+++ demo_pythond_jango/templatedemo/admin.py
@@ -3,6 +3,17 @@ from staticfiledemo.models import AreaInfo
 
 # Register your models here.
 
+#关联对象, , 一对多的关系
+class AreaStackedInline(admin.StackedInline):
+    #写多类的名字
+    model = AreaInfo
+
+
+#表格的嵌入方式,展示方式
+class AreaTabularInline(admin.TabularInline):
+    model = AreaInfo
+    extra=2
+
 #自定义模型管理类
 class AreaInfoAdmin(admin.ModelAdmin):
     '''地区模型管理类'''
@@ -14,5 +25,17 @@ class AreaInfoAdmin(admin.ModelAdmin):
 
     list_filter = ['atitle']    #列表页右侧g过滤栏
     search_fields = ['atitle']  #列表页上方的搜索框
+
+
+    #编辑页字段显示顺序
+    #fields = ['aParent','atitle']
+    #分组显示 , fieldsets和fields只能用一个
+    fieldsets = (
+        ('基本',{'fields':['atitle']}),
+        ('高级', {'fields': ['aParent']})
+    )
+
+    #inlines = [AreaStackedInline]
+    inlines = [AreaTabularInline]
 
 admin.site.register(AreaInfo , AreaInfoAdmin);
