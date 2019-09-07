f395e4b782b16bfa219ae0909144e8c35ad7dfbf
yuezh2 <yuezh2@lenovo.com>
2019-09-07 22:45:02 +0800

    05_后台管理_列表页选项

--- demo_pythond_jango/staticfiledemo/models.py
+++ demo_pythond_jango/staticfiledemo/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class AreaInfo(models.Model):
+    '''地址模型类'''
+    #地区名称
+    atitle = models.CharField(verbose_name='标题',max_length=20);
+    #自关联属性
+    aParent = models.ForeignKey('self',null=True,blank=True);
+
+    #可以用于admin的列表展示
+    def __str__(self):
+        return self.atitle;
+
+
+    def title(self):
+        return self.atitle;
+
+
+    def parent(self):
+        if self.aParent is None:
+            return ''
+        return self.aParent.atitle
+
+
+    #排序
+    title.admin_order_field='atitle'
+
+    title.short_description='地区名称'
+
+    class Meta:
+        db_table = 'booktest_areainfo';  #指定模型类对应的表名
+
--- demo_pythond_jango/templatedemo/admin.py
+++ demo_pythond_jango/templatedemo/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from staticfiledemo.models import AreaInfo
 
 # Register your models here.
+
+#自定义模型管理类
+class AreaInfoAdmin(admin.ModelAdmin):
+    '''地区模型管理类'''
+    list_per_page = 3  #指定每页显示10条数据
+    list_display = ['id' , 'atitle' , 'title' , 'parent'];
+
+    actions_on_bottom = True;
+    actions_on_top = False;
+
+    list_filter = ['atitle']    #列表页右侧g过滤栏
+    search_fields = ['atitle']  #列表页上方的搜索框
+
+admin.site.register(AreaInfo , AreaInfoAdmin);
