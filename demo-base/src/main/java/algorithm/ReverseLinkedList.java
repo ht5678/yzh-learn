1fb88d10f7d15c7137520794ae92b44a1effc9c1
yuezh2 <yuezh2@lenovo.com>
2020-11-09 19:01:52 +0800

    test

---
+++ demo-base/src/main/java/algorithm/ReverseLinkedList.java
@@ -0,0 +1,90 @@
+package algorithm;
+
+
+/**
+ * 
+ * @author yuezh2
+ *
+ * @date 2020年11月9日 下午4:56:16  
+ *
+ */
+public class ReverseLinkedList {
+
+	static class Node{
+		
+		private int data;
+		
+		private Node next;
+		
+		
+		
+		public Node(int data) {
+			this.data = data;
+		}
+		
+		
+
+		public int getData() {
+			return data;
+		}
+
+		public void setData(int data) {
+			this.data = data;
+		}
+
+		public Node getNext() {
+			return next;
+		}
+
+		public void setNext(Node next) {
+			this.next = next;
+		}
+		
+		
+	}
+	
+	
+	
+	
+	public static Node reverse(Node head) {
+		if(head ==null || head.next !=null) {
+			return head;
+		}
+		
+		Node next = head.next;
+		
+		Node newHead = reverse(next);
+		next.next = head;
+		head.next = null;
+		return newHead;
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) throws Exception{
+		Node node1 = new Node(1);
+		Node node2 = new Node(2);
+		Node node3 = new Node(3);
+		Node node4 = new Node(4);
+		Node node5 = new Node(5);
+		
+		node1.setNext(node2);
+		node2.setNext(node3);
+		node3.setNext(node4);
+		node4.setNext(node5);
+		
+		
+		Node head = reverse(node1);
+		
+		while(head!=null) {
+			System.out.println(head.getData());
+			head = head.next;
+		}
+		
+	}
+	
+	
+	
+}
