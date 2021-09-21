cc1ad81ad03d35364d4562b3a404b3200f1f7cc8
yuezhihua <sdwhyzhh@163.com>
2021-09-21 23:15:42 +0800

    test

--- demo-authserver/authserver/src/main/test/com/learn/authserver/TestUtil.java
+++ demo-authserver/authserver/src/main/test/com/learn/authserver/TestUtil.java
@@ -16,7 +16,40 @@ public class TestUtil {
 	
 	public static final ApacheHttpClient HTTPCLIENT = new ApacheHttpClient(10, 5000, 30000, 5 * 1024 * 1024 * 1024, 16, 16);
 	
-
+	
+	/**
+	 * 授权码模式
+	 * 
+	 * 1.http://localhost:9999/oauth/authorize?response_type=code&client_id=portal_app&redirect_uri=http://www.baidu.com&state=abc
+	 * 2.用code换token
+	 * 
+	 */
+	@Test
+	public void test2(){
+		Map<String, String> headers = new HashMap<String, String>();
+//		headers.put("Content-Type", "application/json");
+		headers.put("Content-Type", "application/x-www-form-urlencoded");
+		//portal_app:portal_app
+		headers.put("Authorization", "Basic cG9ydGFsX2FwcDpwb3J0YWxfYXBw");	// 格式    appid:appsecret   然后base64编码
+		
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("code", "Q6nMD4");
+		params.put("grant_type", "authorization_code");
+		params.put("redirect_uri", "http://www.baidu.com");
+		
+		String response = HTTPCLIENT.post("http://localhost:9999/oauth/token", params, headers, "UTF8");
+		System.out.println(response);
+	}
+	
+	
+	
+	/**
+	 * 
+	 * password模式
+	 * 
+	 * 用户名密码换token
+	 * 
+	 */
 	@Test
 	public void test(){
 		Map<String, String> headers = new HashMap<String, String>();
