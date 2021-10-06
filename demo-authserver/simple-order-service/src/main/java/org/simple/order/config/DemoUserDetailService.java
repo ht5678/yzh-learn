c8d414d31fd0c299abdb2fa63b31207af0cfad95
yuezhihua <sdwhyzhh@163.com>
2021-10-06 18:04:39 +0800

    config

---
+++ demo-authserver/simple-order-service/src/main/java/org/simple/order/config/DemoUserDetailService.java
@@ -0,0 +1,37 @@
+package org.simple.order.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * 
+ * @author yue
+ *
+ */
+public class DemoUserDetailService implements UserDetailsService{
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(DemoUserDetailService.class);
+	
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		LOGGER.info("当前登录用户名为 : {}" , username);
+		
+		return User.builder().username(username)
+										.password(passwordEncoder.encode("123456"))
+										.authorities("ROLE_ADMIN").build();
+	}
+	
+	
+	
+}
---
+++ demo-authserver/simple-order-service/src/main/java/org/simple/order/config/WebSecurityConfig.java
@@ -0,0 +1,79 @@
+package org.simple.order.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.DefaultUserAuthenticationConverter;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 
+ * @author yue
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	private DemoUserDetailService demoUserDetailService;
+	
+	
+	/**
+	 * 资源服务器 , 要去资源服务器校验的bean
+	 * @return
+	 */
+	@Bean
+	public ResourceServerTokenServices resourceServerTokenServices(){
+		RemoteTokenServices remoteTokenServices = new RemoteTokenServices();
+		remoteTokenServices.setClientId("order_app");
+		remoteTokenServices.setClientSecret("zhangsan");
+		remoteTokenServices.setCheckTokenEndpointUrl("http://auth-server/oauth/check_token");
+		remoteTokenServices.setRestTemplate(restTemplate);
+		//校验令牌只会拿到用户名 , 但我需要获取到整个的用户信息 , 就需要这个转换器
+//		remoteTokenServices.setAccessTokenConverter(accessTokenConverter());
+		return remoteTokenServices;
+	}
+	
+	
+	
+	
+	/**
+	 * 资源服务器 , 要去资源服务器校验的bean , 该bean会作为属性塞入到
+	 * OAuth2AuthenticationProcessingFilter过滤器 , 该过滤器就会通过AuthenticationManager
+	 * 中的ResourceServerTokenService组件去认证服务器  校验我们的token
+	 * 
+	 */
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		OAuth2AuthenticationManager manager = new OAuth2AuthenticationManager();
+		manager.setTokenServices(resourceServerTokenServices());
+		return manager;
+	}
+	
+	
+	/*@Bean
+	public AccessTokenConverter accessTokenConverter(){
+		DefaultAccessTokenConverter accessTokenConverter = new DefaultAccessTokenConverter();
+		
+		DefaultUserAuthenticationConverter userAuthenticationConverter = new DefaultUserAuthenticationConverter();
+		userAuthenticationConverter.setUserDetailsService(demoUserDetailService);
+		
+		accessTokenConverter.setUserTokenConverter(userAuthenticationConverter);
+		
+		return accessTokenConverter;
+	}*/
+	
+}
