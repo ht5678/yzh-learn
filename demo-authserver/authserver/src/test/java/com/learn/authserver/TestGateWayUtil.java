749ac2340986a45bc14117edf54bcd658d14184c
yuezhihua <sdwhyzhh@163.com>
2021-10-21 15:25:11 +0800

    test

--- demo-authserver/authserver/src/test/java/com/learn/authserver/TestGateWayUtil.java
+++ demo-authserver/authserver/src/test/java/com/learn/authserver/TestGateWayUtil.java
@@ -36,4 +36,34 @@ public class TestGateWayUtil {
 		System.out.println(response);
 	}
 	
+	
+	
+	
+	/**
+	 * 授权码模式
+	 * 
+	 * 1.  http://localhost:9999/oauth/authorize?response_type=code&client_id=portal_app&redirect_uri=http://www.baidu.com&state=abc
+	 * 2.  用code换token
+	 * 
+	 */
+	@Test
+	public void test2(){
+		Map<String, String> headers = new HashMap<String, String>();
+//		headers.put("Content-Type", "application/json");
+		headers.put("Content-Type", "application/x-www-form-urlencoded");
+		//portal_app:portal_app
+		headers.put("Authorization", "Basic cG9ydGFsX2FwcDpwb3J0YWxfYXBw");	// 格式    appid:appsecret   然后base64编码
+		
+		//order_app:order_app  // 只支持password授权模式
+//		headers.put("Authorization", "Basic b3JkZXJfYXBwJTNBb3JkZXJfYXBw");	//
+		
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("code", "2q2YOe");
+		params.put("grant_type", "authorization_code");
+		params.put("redirect_uri", "http://www.baidu.com");
+		
+		
+		String response = HTTPCLIENT.post("http://localhost:9999/oauth/token", params, headers, "UTF8");
+		System.out.println(response);
+	}
 }
