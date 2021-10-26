587f24a89a8b4286ec88d8302724a99520b077ea
yuezhihua <sdwhyzhh@163.com>
2021-10-26 15:05:06 +0800

    domain

--- demo-authserver/sso-portal-password/src/main/java/org/sso/portal/password/entity/ProductInfo.java
+++ demo-authserver/sso-portal-password/src/main/java/org/sso/portal/password/entity/ProductInfo.java
@@ -1,5 +1,6 @@
 package org.sso.portal.password.entity;
 
+import java.math.BigDecimal;
 
 /**
  * 
@@ -7,5 +8,46 @@ package org.sso.portal.password.entity;
  *
  */
 public class ProductInfo {
+	
+	private String productNo;
+	
+	private String productName;
+	
+	private int productStore;
+	
+	private BigDecimal productPrice;
 
+	public String getProductNo() {
+		return productNo;
+	}
+
+	public void setProductNo(String productNo) {
+		this.productNo = productNo;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public int getProductStore() {
+		return productStore;
+	}
+
+	public void setProductStore(int productStore) {
+		this.productStore = productStore;
+	}
+
+	public BigDecimal getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(BigDecimal productPrice) {
+		this.productPrice = productPrice;
+	}
+	
+	
 }
