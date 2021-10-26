0e3995e8bf2ff742ec158774e5fe1daade2f50d0
yuezhihua <sdwhyzhh@163.com>
2021-10-26 15:30:28 +0800

    test

--- demo-authserver/gateway-product-service/src/main/java/org/gateway/product/controller/ProductInfoController.java
+++ demo-authserver/gateway-product-service/src/main/java/org/gateway/product/controller/ProductInfoController.java
@@ -1,5 +1,9 @@
 package org.gateway.product.controller;
 
+import org.simple.base.dao.entity.ProductInfo;
+import org.simple.base.dao.mapper.ProductInfoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/product")
 public class ProductInfoController {
 
+	@Autowired
+	private ProductInfoMapper productInfoMapper;
 	
+	
+	@RequestMapping("/selectProductInfoById/{productNo}")
+	public Object selectProductInfoById(@PathVariable("productNo")String productNo) {
+		ProductInfo productInfo = productInfoMapper.selectProductInfoById(productNo);
+		return productInfo;
+	}
 	
 }
