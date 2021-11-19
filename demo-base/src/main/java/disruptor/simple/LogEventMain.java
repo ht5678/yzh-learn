6313ae72f8627b94744f5113f6d3e93829ae3e7a
yuezhihua <sdwhyzhh@163.com>
2021-11-19 16:10:14 +0800

    note

--- demo-base/src/main/java/disruptor/simple/LogEventMain.java
+++ demo-base/src/main/java/disruptor/simple/LogEventMain.java
@@ -27,9 +27,22 @@ public class LogEventMain {
 		//环形数组的容量 . 必须是2的倍数
 		int bufferSize = 1024;
 		
+		
+		//com.lmax.disruptor.BlockingWaitStrategy		default
+		//最低效的策略，但其对CPU的消耗最小，并且在各种部署环境中能提供更加一致的性能表现；
+		//内部维护了一个重入锁ReentrantLock和Condition；
+		
+		//com.lmax.disruptor.SleepingWaitStrategy
+		//性能表现和com.lmax.disruptor.BlockingWaitStrategy差不多，对CPU的消耗也类似，但其对生产者线程的影响最小，适合用于异步日志类似的场景；
+		//是一种无锁的方式；
+		
+		//com.lmax.disruptor.YieldingWaitStrategy
+		//性能最好，适合用于低延迟的系统；在要求极高性能且事件处理线程数小于CPU逻辑核心树的场景中，推荐使用此策略；例如，CPU开启超线程的特性；
+		//也是无锁的实现，只要是无锁的实现，signalAllWhenBlocking()都是空实现；
+		
 		//构造Disruptor
 		Disruptor<LogEvent> disruptor = new Disruptor<>(factory, bufferSize, DaemonThreadFactory.INSTANCE , ProducerType.SINGLE , 
-									new YieldingWaitStrategy());
+									new YieldingWaitStrategy());//决定一个消费者如何等待生产者将Event置入Disruptor；	其所有实现都是针对消费者线程的；
 		
 		//设置消费者
 		disruptor.handleEventsWith(new LogEventConsumer());
