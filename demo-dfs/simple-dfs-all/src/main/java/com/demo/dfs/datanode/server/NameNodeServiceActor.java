2a3fb0cfe30b97f3188546ad54d2dcc97cbfc5f8
yuezhihua <sdwhyzhh@163.com>
2021-12-21 21:35:01 +0800

    datanode

--- demo-dfs/simple-dfs-all/src/main/java/com/demo/dfs/datanode/server/NameNodeServiceActor.java
+++ demo-dfs/simple-dfs-all/src/main/java/com/demo/dfs/datanode/server/NameNodeServiceActor.java
@@ -10,8 +10,39 @@ import java.util.concurrent.CountDownLatch;
 public class NameNodeServiceActor {
 
 	
-	
+	/**
+	 * 向自己负责通信的那个NameNode进行扫描
+	 * @param latch
+	 */
 	public void register(CountDownLatch latch) {
+		Thread registerThread = new RegisterThread(latch);
+		registerThread.start();
+	}
+	
+	
+	/**
+	 * 负责注册的线程
+	 * @author yuezh2@lenovo.com
+	 *	@date 2021年12月21日下午9:31:10
+	 */
+	class RegisterThread extends Thread{
+		CountDownLatch latch;
+		
+		public RegisterThread(CountDownLatch latch) {
+			this.latch = latch;
+		}
+
+		@Override
+		public void run() {
+			try {
+				//发送rpc接口请求到NameNode进行注册
+				System.out.println("发送请求到NameNode进行注册 ... ... ");
+				Thread.sleep(1000);
+				latch.countDown();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
 		
 	}
 
