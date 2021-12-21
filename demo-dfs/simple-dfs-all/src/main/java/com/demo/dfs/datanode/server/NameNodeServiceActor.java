6b962ed5e8819803da16dfdbd622cc66d58ffd7b
yuezhihua <sdwhyzhh@163.com>
2021-12-21 21:28:59 +0800

    datanode

---
+++ demo-dfs/simple-dfs-all/src/main/java/com/demo/dfs/datanode/server/NameNodeServiceActor.java
@@ -0,0 +1,18 @@
+package com.demo.dfs.datanode.server;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * 
+ * @author yuezh2@lenovo.com
+ *	@date 2021年12月21日下午9:15:42
+ */
+public class NameNodeServiceActor {
+
+	
+	
+	public void register(CountDownLatch latch) {
+		
+	}
+
+}
