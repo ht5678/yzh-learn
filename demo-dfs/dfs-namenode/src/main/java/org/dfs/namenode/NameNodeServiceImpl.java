46cb2512037544c5de7a5f6cab6627085bbf5065
yuezhihua <sdwhyzhh@163.com>
2021-12-28 18:38:16 +0800

    init

--- demo-dfs/dfs-namenode/src/main/java/org/dfs/namenode/NameNodeServiceImpl.java
+++ demo-dfs/dfs-namenode/src/main/java/org/dfs/namenode/NameNodeServiceImpl.java
@@ -1,13 +1,19 @@
 package org.dfs.namenode;
 
-import com.demo.dfs.namenode.service.NameNodeServer;
+import com.demo.dfs.rpc.model.HeartbeatRequest;
+import com.demo.dfs.rpc.model.HeartbeatResponse;
+import com.demo.dfs.rpc.model.RegisterRequest;
+import com.demo.dfs.rpc.model.RegisterResponse;
+import com.demo.dfs.rpc.service.NameNodeServiceGrpc.NameNodeService;
+
+import io.grpc.stub.StreamObserver;
 
 /**
  * NameNode的rpc服务接口
  * @author yuezh2@lenovo.com
  *	@date 2021年12月21日下午8:58:04
  */
-public class NameNodeServiceImpl implements NameNodeServer {
+public class NameNodeServiceImpl implements NameNodeService {
 	
 	/**
 	 * 负责管理元数据的核心组件
@@ -54,6 +60,20 @@ public class NameNodeServiceImpl implements NameNodeServer {
 	 */
 	public void start() {
 		System.out.println("开始监听指定的rpc server端口号 , 来接收请求");
+	}
+
+
+	@Override
+	public void register(RegisterRequest request, StreamObserver<RegisterResponse> responseObserver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void heartbeat(HeartbeatRequest request, StreamObserver<HeartbeatResponse> responseObserver) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
---
+++ demo-dfs/dfs-rpc/src/main/java/com/demo/dfs/rpc/App.java
@@ -0,0 +1,13 @@
+package com.demo.dfs.rpc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
