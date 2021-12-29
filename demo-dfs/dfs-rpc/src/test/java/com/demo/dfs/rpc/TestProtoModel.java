1c199f94238a9f569f32f93f802837eb4ea75883
yuezhihua <sdwhyzhh@163.com>
2021-12-29 15:48:20 +0800

    test proto model

--- demo-dfs/dfs-rpc/src/test/java/com/demo/dfs/rpc/TestProtoModel.java
+++ demo-dfs/dfs-rpc/src/test/java/com/demo/dfs/rpc/TestProtoModel.java
@@ -1,18 +1,28 @@
 package com.demo.dfs.rpc;
 
 import com.demo.dfs.rpc.model.RegisterRequest;
+import com.googlecode.protobuf.format.JsonFormat;
 
 /**
- * Hello world!
- *
+ * 
+ * @author yuezh2@lenovo.com
+ *	@date 2021年12月29日下午3:07:49
  */
-public class App {
+public class TestProtoModel {
+
     public static void main( String[] args )throws Exception{
     	
     	RegisterRequest rr = RegisterRequest.newBuilder().setHostname("test").build();
     	byte[] bytes = rr.toByteArray();
     	
+    	//byte
     	RegisterRequest rr1 = RegisterRequest.parseFrom(bytes);
     	System.out.println(rr1.getHostname());
+    	
+    	//json
+    	JsonFormat format = new JsonFormat();
+    	String json = format.printToString(rr);
+    	System.out.println(json);
     }
+	
 }
