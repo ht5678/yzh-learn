8bbcd8e5cfd496d2da54c7941e453a083d8d60d6
yuezhihua <sdwhyzhh@163.com>
2022-04-13 18:32:10 +0800

    imv1

---
+++ demo-oc/demo-netty/src/main/java/org/demo/netty/im/simple/v1/im/handler/PollingTransport.java
@@ -0,0 +1,197 @@
+package org.demo.netty.im.simple.v1.im.handler;
+
+import org.demo.netty.im.fake.domain.Transport;
+import org.demo.netty.im.fake.im.bs.config.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+//import java.util.concurrent.TimeUnit;
+//
+//import org.demo.netty.im.fake.domain.AddressFrom;
+//import org.demo.netty.im.fake.domain.Packet;
+//import org.demo.netty.im.fake.domain.PacketType;
+//import org.demo.netty.im.fake.domain.Transport;
+//import org.demo.netty.im.fake.exception.BSAuthorizeException;
+//import org.demo.netty.im.fake.im.OCIMServer;
+//import org.demo.netty.im.fake.im.bs.config.Configuration;
+//import org.demo.netty.im.fake.im.bs.message.OutPacketMessage;
+//import org.demo.netty.im.fake.im.bs.message.PollOkMessage;
+//import org.demo.netty.im.fake.im.bs.message.PongPacketMessage;
+//import org.demo.netty.im.fake.im.cs.chain.PacketChain;
+//import org.demo.netty.im.fake.scheduler.SchedulerKey;
+//import org.demo.netty.im.fake.scheduler.SchedulerKey.SchedulerType;
+//import org.demo.netty.im.fake.session.CustomerSession;
+//import org.demo.netty.im.fake.util.HttpResponses;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import io.netty.channel.Channel;
+//import io.netty.channel.ChannelFutureListener;
+//import io.netty.channel.ChannelHandlerContext;
+//import io.netty.channel.ChannelInboundHandlerAdapter;
+//import io.netty.handler.codec.http.DefaultHttpResponse;
+//import io.netty.handler.codec.http.HttpResponse;
+//import io.netty.handler.codec.http.HttpResponseStatus;
+//import io.netty.handler.codec.http.HttpVersion;
+//import io.netty.util.ReferenceCountUtil;
+
+/**
+ * 
+ * @author yuezh2
+ * @date	  2022年3月29日 下午10:08:02
+ */
+public class PollingTransport extends ChannelInboundHandlerAdapter{
+
+	private Logger log = LoggerFactory.getLogger(PollingTransport.class);
+	
+
+	public final static Transport TRANSPORT = Transport.POLLING;
+	private final Configuration config;
+	
+	private final static Long POLL_CONNECT_MILLISECOND = 60 * 1000L;
+	
+	
+	/**
+	 * 
+	 * @param config
+	 */
+	public PollingTransport(Configuration config) {
+		this.config = config;
+	}
+
+
+	
+	
+	/**
+	 * 
+	 */
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		System.out.println(msg);
+//		Channel channel = ctx.channel();
+//		if(msg instanceof Packet) {
+//			Packet packet = (Packet)msg;
+//			Transport ts = packet.getTs();
+//			
+//			if(TRANSPORT == ts) {
+//				try {
+//					switch (packet.getType()) {
+//					case POLL:
+//						onPoller(channel, packet);
+//						break;
+//					case MESSAGE:
+//						onSender(channel, packet);
+//						break;
+//					case PING:
+//						pong(channel, packet);
+//						break;
+//					case CLOSE:
+//						close(channel, packet);
+//						break;
+//					default:
+//						HttpResponses.sendBadRequestError(channel);
+//						log.warn("客户发送的消息 , 不符合规范 . packet : {} " , packet);
+//					}
+//				}catch(Exception e) {
+//					log.error("Http deal message error : {}" , e.getMessage());
+//					e.printStackTrace();
+//					HttpResponses.sendBadRequestError(channel);
+//				}finally {
+//					ReferenceCountUtil.release(msg);
+//				}
+//			}else {
+//				//传递到下一个handler , handler是链式的 , 可能有多个 , 然后原有的消息需要释放, ReferenceCountUtil.release(msg);
+//				//A处理器把msg透传给了B，当然了，A不一定直接透传，也可以传递处理过的消息。				
+//				ctx.fireChannelRead(msg);
+//			}
+//		}else {
+//			//传递到下一个handler , handler是链式的 , 可能有多个 , 然后原有的消息需要释放, ReferenceCountUtil.release(msg);
+//			//A处理器把msg透传给了B，当然了，A不一定直接透传，也可以传递处理过的消息。
+//			ctx.fireChannelRead(msg);
+//		}
+	}
+	
+	
+	
+//	/**
+//	 * 
+//	 * @param channel
+//	 * @param packet
+//	 */
+//	private void close(Channel channel , Packet packet) {
+//		AddressFrom from = packet.getFrom();
+//		CustomerSession customerSession = OCIMServer.getInst().getRoutingTable().getLocalCustomerSession(from.getUid());
+//		if(null == customerSession) {
+//			log.info("客户 :{} , 已经离开不需要再次处理" , from.getUid());
+//		}else {
+//			customerSession.disconnect();
+//			channel.close();
+//			log.info("客户: {} , Http-close 直接关闭会话" , from.getUid());
+//		}
+//	}
+//	
+//	
+//	
+//	/**
+//	 * 
+//	 */
+//	private void onSender(Channel channel , Packet packet) throws Exception {
+//		AddressFrom from = packet.getFrom();
+//		CustomerSession customerSession = OCIMServer.getInst().getRoutingTable().getLocalCustomerSession(from.getUid());
+//		if(customerSession == null) {
+//			sendError(channel);
+//			log.error("检测用户 : {} , 会话不存在" , packet.getCid());
+//			return;
+//		}
+//		channel.writeAndFlush(new PollOkMessage());
+//		PacketChain.dispatcherCustomerPacket(customerSession, packet);
+//	}
+//	
+//	
+//	
+//	/**
+//	 * 
+//	 */
+//	private void sendError(Channel channel) {
+//		HttpResponse resp = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.INTERNAL_SERVER_ERROR);
+//		channel.writeAndFlush(resp).addListener(ChannelFutureListener.CLOSE);
+//	}
+//	
+//	
+//	
+//	
+//	/**
+//	 * 
+//	 */
+//	private void onPoller(Channel channel , Packet packet) throws BSAuthorizeException {
+//		AddressFrom from = packet.getFrom();
+//		CustomerSession session = OCIMServer.getInst().getRoutingTable().getLocalCustomerSession(from.getUid());
+//		if(null == session) {
+//			throw new BSAuthorizeException("未获取到当前用户状态");
+//		}
+//		
+//		session.bindChannel(channel);
+//		//保证在重连过程中,消息有缓存
+//		channel.writeAndFlush(new OutPacketMessage(session));
+//		SchedulerKey key = new SchedulerKey(SchedulerType.PING_TIMEOUT , channel);
+//		OCIMServer.getInst().getScheduler().cancel(key);
+//		OCIMServer.getInst().getScheduler().scheduler(key, () -> channel.writeAndFlush(new PollOkMessage()), POLL_CONNECT_MILLISECOND , TimeUnit.MILLISECONDS);
+//	}
+//	
+//	
+//	
+//	/**
+//	 * 
+//	 * @param channel
+//	 * @param packet
+//	 */
+//	private void pong(Channel channel, Packet packet) {
+//		Packet pongPacket = new Packet(PacketType.PONG);
+//		pongPacket.setFrom(packet.getFrom());
+//		channel.writeAndFlush(new PongPacketMessage(pongPacket));
+//	}
+	
+}
---
+++ demo-oc/demo-netty/src/main/java/org/demo/netty/im/simple/v1/im/handler/WebsocketTransport.java
@@ -0,0 +1,220 @@
+package org.demo.netty.im.simple.v1.im.handler;
+
+import org.demo.netty.im.fake.domain.Transport;
+import org.demo.netty.im.fake.im.bs.config.Configuration;
+import org.demo.netty.im.fake.im.coder.PacketEncoder;
+import org.demo.netty.im.fake.util.PacketDecoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * 
+ * @author yuezh2
+ * @date	  2022年4月13日 下午6:17:11
+ */
+public class WebsocketTransport extends ChannelInboundHandlerAdapter{
+
+	private final static Logger log = LoggerFactory.getLogger(WebsocketTransport.class);
+	
+	private static final String HEART = "heart";
+	private final static Transport TRANSPORT = Transport.WEBSOCKET;
+	
+	private final boolean isSsl;
+	private final Configuration config;
+	private PacketDecoder decoder;
+	private PacketEncoder encoder;
+	
+	
+	/**
+	 * 
+	 */
+	public WebsocketTransport(Configuration config , boolean isSsl , PacketEncoder encoder , PacketDecoder decoder) {
+		this.isSsl = isSsl;
+		this.config = config;
+		this.encoder = encoder;
+		this.decoder = decoder;
+	}
+
+
+	
+	
+	/**
+	 * 
+	 */
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object obj) throws Exception {
+		
+		System.out.println(obj);
+		
+//		Channel channel = ctx.channel();
+//		Session session = getChannelSession(channel);
+//		if(null == session) {
+//			ReferenceCountUtil.release(obj);
+//			ctx.channel().close();
+//			throw new IllegalAccessError("无效的客户端连接,服务器关闭和当前客户端连接");
+//		}
+//		
+//		//
+//		if(obj instanceof CloseWebSocketFrame) {
+//			if(session instanceof LocalCustomerSession) {
+//				log.info("客户主动关闭当前会话 Uid : {}" , session.getUid());
+//			}else {
+//				log.info("客服退出登录Uid : {}" , session.getUid());
+//			}
+//			ReferenceCountUtil.release(obj);
+//		}else if(obj instanceof TextWebSocketFrame) {
+//			TextWebSocketFrame frame = (TextWebSocketFrame)obj;
+//			try {
+//				resolverTextWebSocketFrame(session, channel, frame.text());
+//			} finally {
+//				frame.release();
+//			}
+//		}else if(obj instanceof FullHttpRequest) {
+//			FullHttpRequest request = (FullHttpRequest)obj;
+//			try {
+//				if(session.getTransport() == TRANSPORT) {
+//					upgradeWebSocket(ctx, session, request);
+//				}else {
+//					ctx.channel().close();
+//					throw new IllegalAccessException("不支持的通讯协议: " + session.getTransport());
+//				}
+//			}finally {
+//				request.release();
+//			}
+//		}else {
+//			ctx.fireChannelRead(obj);
+//		}
+	}
+	
+	
+	
+//	/**
+//	 * 处理消息
+//	 */
+//	private void resolverTextWebSocketFrame(Session session , Channel channel , String message) {
+//		Packet packet = null;
+//		try {
+//			packet = decoder.decodePackets(message, Packet.class);
+//			if(packet.getType() == PacketType.PING) {
+//				HeartDetector.pongBs(session);
+//				return;
+//			}
+//			
+//			//客服消息
+//			if(session instanceof WaiterSession) {
+//				log.info("客服消息 packet : {}" , packet);
+//				WaiterSession waiterSession = (WaiterSession)session;
+//				switch (packet.getType()) {
+//				case TRANSFER:
+//					TransferChain.transfer(packet);
+//					break;
+//				case CLOSE_CHAT:
+//					CloseChatChain.closeDispatcher(packet);
+//					break;
+//				case RECEPTION:
+//					ReceptionChain.directReception(waiterSession, packet);
+//					break;
+//				case CHANGE_STATUS:
+//					WaiterStatusChain.changeStatus(waiterSession, packet);
+//					break;
+//				case BIND:
+//					BindChain.waiterBind(waiterSession, channel, encoder);
+//					break;
+//				case REVOCATION:
+//					RevocationChain.revocation(waiterSession, packet);
+//					break;
+//				default:
+//					PacketChain.dispatcherWaiterPacket(waiterSession, packet);
+//					break;
+//				}
+//			}else if(session instanceof CustomerSession) {
+//				log.info("客户消息 packet : {}" , packet);
+//				CustomerSession customerSession = (CustomerSession)session;
+//				PacketChain.dispatcherCustomerPacket(customerSession, packet);
+//			}else {
+//				log.error("无法分发的消息 : {}" , packet);
+//			}
+//		}catch(Exception e) {
+//			log.warn("packet : {}  解析数据发生错误" , packet);
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	
+//	
+//	/**
+//	 * http升级  web socket
+//	 */
+//	private void upgradeWebSocket(ChannelHandlerContext ctx , Session session , FullHttpRequest req) {
+//		final Channel channel = ctx.channel();
+//		WebSocketServerHandshakerFactory factory = new WebSocketServerHandshakerFactory(getWebsocketUrl(req) , null , true , config.getMaxFramePayloadLength());
+//		WebSocketServerHandshaker handshake = factory.newHandshaker(req);
+//		
+//		if(handshake != null) {
+//			handshake.handshake(channel, req , getResponseHeaders(session) , ctx.newPromise()).addListener((ChannelFutureListener) future -> {
+//					if(future.isSuccess()) {
+//						channel.pipeline().addFirst(HEART , new IdleStateHandler(60, 0, 0 , TimeUnit.SECONDS));
+//						channel.pipeline().addBefore(BsChannelInitializer.WEB_SOCKET_TRANSPORT, 
+//								BsChannelInitializer.WEB_SOCKET_AGGREGATOR, 
+//								new WebSocketFrameAggregator(config.getMaxFramePayloadLength()));
+//					}else {
+//						log.error("uid : {}  握手失败 : {} " , session.getUid() , future.cause());
+//					}
+//			});
+//		}else {
+//			WebSocketServerHandshakerFactory.sendUnsupportedVersionResponse(channel);
+//		}
+//		
+//	}
+//	
+//	
+//	/**
+//	 * 获取协议
+//	 * @return
+//	 */
+//	private String getWebsocketUrl(HttpRequest req) {
+//		String protocol = "ws://";
+//		if(isSsl) {
+//			protocol = "wss://";
+//		}
+//		return protocol+req.headers().get(HttpHeaderNames.HOST) + req.uri();
+//	}
+//	
+//	
+//	
+//	/**
+//	 * 设置 cookies
+//	 * @return
+//	 */
+//	private HttpHeaders getResponseHeaders(Session session) {
+//		if(session instanceof WaiterSession) {
+//			WaiterSession waiterSession = (WaiterSession)session;
+//			Waiter waiter = waiterSession.getWaiter();
+//			String sign = WaiterAuthCoder.encode(waiter);
+//			DefaultCookie cookie = new DefaultCookie(WaiterAuthCoder.SERVER_COOKIE_NAME, sign);
+//			cookie.setPath("/");
+//			cookie.setHttpOnly(false);
+//			cookie.setSecure(false);
+//			final DefaultHttpHeaders httpHeaders = new DefaultHttpHeaders();
+//			httpHeaders.add(HttpHeaderNames.SET_COOKIE , ServerCookieEncoder.LAX.encode(cookie));
+//			return httpHeaders;
+//		}
+//		return new DefaultHttpHeaders();
+//	}
+//	
+//	
+//	
+//	
+//	/**
+//	 * 获取当前session
+//	 * @return
+//	 */
+//	private Session getChannelSession(Channel channel) {
+//		return channel.attr(Session.CLIENT_SESSION).get();
+//	}
+	
+	
+}
