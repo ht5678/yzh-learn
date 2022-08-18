46b1e78d3fdad62e47d6411ad1d520c44489ce17
yuezhihua <sdwhyzhh@163.com>
2022-08-19 00:41:26 +0800

    demo im init
    demo im init

---
+++ demo-im/demo-im-gateway-tcp/src/main/java/org/demo/im/gateway/tcp/App.java
@@ -0,0 +1,13 @@
+package org.demo.im.gateway.tcp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
